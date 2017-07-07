5ef87cc0a94a903bc8233d0142582ac617908ec9
Rohit Sharma <rs@gmail.com>
2017-07-07 01:00:37 -0400

    Passport Setup

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-var express = require('express');
+ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passpot');
+var session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: '552607021339-nma6l0u7qdg7a9g13jsa12ea0k6tde0f.apps.googleusercontent.com',
+    clientSecret : 'My0kZ2l2Q0wt8NlKVOH5_sGM',
+    callbackURL: 'http://localhost:3000/auth/google/callback'},
+    function(req, accessToken, refreshToken, profile, done) {
+        done(null, profile);
+    }
+));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +33,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({secret : 'anything'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done){
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+   done(null, user);
+});
 
 app.use('/', index);
 app.use('/users', users);
